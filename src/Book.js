c65d9ebff7730d78d50c2602f0d86583d93823ce
martinseanhunt <martinseanhunt@gmail.com>
2017-11-06 14:44:03 -0500

    check for empty image before trying to render it

--- src/Book.js
+++ src/Book.js
@@ -5,11 +5,10 @@ export default function Book(props) {
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks && props.book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
             <select defaultValue={props.book.shelf} onChange={e => props.onSelectShelf(e, props.book)}>
-              {/* If the book.shelf is not set move to will be selected */}
-              <option value="none" disabled={props.book.shelf}>Move to...</option>
+              <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
